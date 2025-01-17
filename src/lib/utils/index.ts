05093006fcd1669018386c922168a8be0603650f
Luca <lucanahtsu@gmail.com>
2025-01-16 22:49:30 -0300

    fix: Update import paths for utility functions to streamline code

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function arraysEqual(a: number[], b: number[]): boolean {
+  return a.length === b.length && a.every((val, index) => val === b[index])
+}
