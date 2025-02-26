7bbba04834a1a65cd0f1381348ed4840a6f089bd
Luca <lucanahtsu@gmail.com>
2025-02-25 21:51:46 -0300

    feat(UI): Add Keyboard component and replace Octave instances in index.astro

---
+++ src/components/ui/keyboard/DamperFelt.tsx
@@ -0,0 +1,13 @@
+const DamperFelt = () => {
+  return (
+    <div
+      aria-hidden
+      className="w-full h-3 bg-gradient-to-b from-red-500 to-red-700 shadow-inner rounded-t-md"
+      style={{
+        boxShadow: 'inset 0 4px 6px rgba(0, 0, 0, 0.2)'
+      }}
+    />
+  )
+}
+
+export default DamperFelt
---
+++ src/components/ui/keyboard/Keyboard.tsx
@@ -0,0 +1,20 @@
+import DamperFelt from './DamperFelt';
+import Octave from './Octave'
+interface KeyboardProps {
+  octaves?: number;
+}
+
+const Keyboard: React.FC<KeyboardProps> = ({ octaves = 2 }) => {
+  return (
+    <div>
+      <DamperFelt />
+      <div className="flex items-center justify-center">
+        {Array.from({ length: octaves }, (_, index) => (
+          <Octave key={index} />
+        ))}
+      </div>
+    </div>
+  )
+};
+
+export default Keyboard;
