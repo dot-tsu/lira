b25d09a1605a5f8655cbbd6922b1808db8c43039
Luca <lucanahtsu@gmail.com>
2025-03-01 23:32:56 -0300

    style(DamperFelt): Change color to stone

--- src/components/ui/keyboard/DamperFelt.tsx
+++ src/components/ui/keyboard/DamperFelt.tsx
@@ -2,7 +2,7 @@ const DamperFelt = () => {
   return (
     <div
       aria-hidden
-      className="w-full h-3 bg-gradient-to-b from-red-500 to-red-700 shadow-inner rounded-t-md"
+      className="w-full h-3 bg-gradient-to-b from-stone-500 to-stone-700 shadow-inner rounded-t-md"
       style={{
         boxShadow: 'inset 0 4px 6px rgba(0, 0, 0, 0.2)'
       }}
