d87edcc712911959df610e85ec0f1a0e07991b0a
Luca <lucanahtsu@gmail.com>
2025-03-04 17:15:05 -0300

    chore: WIP quality selector

--- src/components/ui/chords/ChordSelector/ChordSelector.tsx
+++ src/components/ui/chords/ChordSelector/ChordSelector.tsx
@@ -1,14 +1,24 @@
-import type Chord from '@/lib/types/chord'
+import type ChordType from '@/lib/types/chord'
 import type Note from '@/lib/types/note'
+import QualitySelect from './QualitySelect'
+
+const COLOR = 'orange'
 
 const ChordSelector = ({
   root,
   onChordChange
 }: {
   root: Note
-  onChordChange?: (chord: Chord) => void
+  onChordChange?: (chord: ChordType) => void
 }) => {
-  return <div>{root.letter}</div>
+  return (
+    <div className='space-y-8'>
+      <QualitySelect quality={'major'} onChange={() => {}} />
+      {/* <SuspensionSelect value={suspended} onChange={setSuspended} />
+      <ExtensionSelect value={extension} onChange={setExtension} />
+      <AddedNotesSelect values={added} onChange={setAdded} /> */}
+    </div>
+  )
 }
 
 export default ChordSelector
---
+++ src/components/ui/chords/ChordSelector/QualitySelect.tsx
@@ -0,0 +1,18 @@
+import type { ChordQuality } from "@/lib/types/chord"
+
+const QualitySelect = ({
+  quality,
+  onChange
+}: {
+  quality: ChordQuality
+  onChange: (q: string) => void
+}) => {
+  return (
+    <div>
+      Quality
+      
+    </div>
+  )
+}
+
+export default QualitySelect
