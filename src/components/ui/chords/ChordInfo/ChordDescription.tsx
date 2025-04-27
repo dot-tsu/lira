e9d5f600142bf43df7631af8c271738c90d5ab87
Luca <lucanahtsu@gmail.com>
2025-04-27 20:58:01 -0300

    feat(ChordDescription): Add prompt for user suggestions when chord description is missing

--- src/components/ui/chords/ChordInfo/ChordDescription.tsx
+++ src/components/ui/chords/ChordInfo/ChordDescription.tsx
@@ -1,7 +1,19 @@
 import type Chord from '@/lib/types/chord'
 
 const ChordDescription = ({ chord }: { chord: Chord }) => {
-  if (!chord.description) return null
+  if (!chord.description) {
+    return (
+      <div className="text-lg font-heading text-center max-w-xl text-neutral-500">
+        <p>We haven't written about this chord yet, but we'd love to hear what you think about it!   <a
+          href={`mailto:lucanahtsu@gmail.com?subject=Suggestion for ${chord.symbol.full} chord description`}
+          className="text-neutral-500 hover:text-neutral-700 underline mt-2 inline-block transition-colors duration-300"
+        >
+          Suggest a description ↗
+        </a></p>
+
+      </div>
+    )
+  }
 
   return (
     <div className="text-lg font-heading text-center max-w-xl">
--- src/components/ui/chords/ChordInfo/ChordNoteDetails.tsx
+++ src/components/ui/chords/ChordInfo/ChordNoteDetails.tsx
@@ -11,15 +11,17 @@ const ChordNoteDetails = ({ chord }: { chord: ChordType }) => {
   const noteLetters = chord.notes.map(note => note.letter + (note.accidental ?? ''))
 
   return (
-    <div className='flex items-center text-center font-heading text-xl text-neutral-600 text-sm h-8'>
-      <Shapes className='mr-2' />
-      <span className='min-w-[60px]'>
-        {intervalDisplay}
-      </span>
-      <span className='text-neutral-400 mx-1'>&bull;</span>
-      <span className={`text-${COLOR}-500 min-w-[80px]`}>
-        ({noteLetters.join(', ')})
-      </span>
+    <div className='flex flex-col items-center text-center font-heading text-xl text-neutral-600 text-sm h-8'>
+      <div className='flex items-center'>
+        <Shapes className='mr-2' />
+        <span className='min-w-[60px]'>
+          {intervalDisplay}
+        </span>
+        <span className='text-neutral-400 mx-1'>&bull;</span>
+        <span className={`text-${COLOR}-500 min-w-[80px]`}>
+          ({noteLetters.join(', ')})
+        </span>
+      </div>
     </div>
   )
 }
